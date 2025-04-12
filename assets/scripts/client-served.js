9948d7dcc6676e72f6bb2e10463aefbc45a81a90
sk-shadik <shadik.aiub@gmail.com>
2025-04-12 22:50:53 +0600

    Add A Successfull Served Client Section

---
+++ assets/scripts/client-served.js
@@ -0,0 +1,17 @@
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / 200;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 15);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
